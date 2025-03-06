0f4170f7b63079a104f486d336c615ccd13fde38
arya-kmar <pilotarya.k@gmail.com>
2025-03-06 06:09:13 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+console.log("Hello")
+
+let name = "Arya"; //This is string variable.
+let age = 18; //This is number variable
+let isLoggedIn = true; //This is boolean variable.
+
+//Primitive variables
+
+    //number -> 2^53.
+    //strings -> use '' or ""(prefferred).
+    //boolean -> true or false;
+    // null -> standalone value(disccussed later)(it's a objesct not a type)
+    // undefined -> not defining a value to the variable(type is assigned)
+    //symbols => unique.
